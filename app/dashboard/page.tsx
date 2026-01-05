888158b7017a1fc0c3f4a61214f20746e8378ad5
adarshauspicious-art <adarsauspicious@gmail.com>
2026-01-05 18:22:30 +0530

    worked on sidebar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function DashboardPage() {
   const router = useRouter();
@@ -30,23 +31,172 @@ export default function DashboardPage() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <>
 
-      <div className="bg-white p-10 rounded-2xl shadow-lg w-full max-w-md text-center">
+      <div className="ml-3 mt-1 fixed h-screen bg-white p-3 shadow-lg rounded-2xl border border-gray-300 w-[300px]">
+  <aside className="sidebar relative text-black">
 
-        <h1 className="text-2xl font-bold mb-4 login-title">
-          Welcome to Dashboard 🎉
-        </h1>
+    {/* Header Row */}
+    <div className="flex items-center justify-between mb-6 mt-2">
+      <Image
+        src="/rofl_img.png"
+        alt="ROFL Logo"
+        width={120}
+        height={40}
+        className="object-contain"
+      />
 
-        <p className="text-gray-600 mb-6">You are successfully logged in.</p>
-
-        <button
-          onClick={handleLogout}
-          className="mt-4 w-full text-white bg-[#F2482D] hover:bg-[#d33c25] py-3 rounded-xl border border-black transition font-semibold flex items-center justify-center gap-2 shadow-[3px_3px_0px_black]"
-        >
-          Logout
-        </button>
-      </div>
+      <Image
+        src="/sidebar.svg"
+        alt="Sidebar Toggle"
+        width={25}
+        height={25}
+        className="cursor-pointer"
+      />
     </div>
+          <ul>
+            <li
+              className="mt-4 ml-2 w-full bg-white py-3 rounded-xl border-none 
+             font-normal text-black text-lg 
+             flex items-center  gap-2
+             hover:text-white hover:bg-[#F2482D]
+             hover:shadow-[3px_3px_0px_black]">
+              <Image
+                src="/Dashboard.svg"
+                alt="Dashboard Icon"
+                width={25}
+                height={25}
+                className=" top-6 left-3"
+              />
+              Dashboard</li>
+
+
+            <li className="mt-4 ml-2 w-full bg-white py-3 rounded-xl border-none 
+             font-normal text-black text-lg 
+             flex items-center  gap-2
+             hover:text-white hover:bg-[#F2482D]
+             hover:shadow-[3px_3px_0px_black]">
+              <Image
+                src="/user_logo.png"
+                alt="Dashboard Icon"
+                width={25}
+                height={25}
+                className=" top-6 left-3"
+              />Sellers</li>
+
+            <li className="mt-4 ml-2 w-full bg-white py-3 rounded-xl border-none 
+             font-normal text-black text-lg 
+             flex items-center  gap-2
+             hover:text-white hover:bg-[#F2482D]
+             hover:shadow-[3px_3px_0px_black]">
+              <Image
+                src="/items.svg"
+                alt="Dashboard Icon"
+                width={25}
+                height={25}
+                className=" top-6 left-3"
+              />Items</li>
+
+            <li className="mt-4 ml-2 w-full bg-white py-3 rounded-xl border-none 
+             font-normal text-black text-lg 
+             flex items-center  gap-2
+             hover:text-white hover:bg-[#F2482D]
+             hover:shadow-[3px_3px_0px_black]">
+              <Image
+                src="/user_logo.png"
+                alt="Dashboard Icon"
+                width={25}
+                height={25}
+                className=" top-6 left-3"
+              />Users</li>
+
+            <li className="mt-4 ml-2 w-full bg-white py-3 rounded-xl border-none 
+             font-normal text-black text-lg 
+             flex items-center  gap-2
+             hover:text-white hover:bg-[#F2482D]
+             hover:shadow-[3px_3px_0px_black]">
+              <Image
+                src="/winners.svg"
+                alt="Dashboard Icon"
+                width={25}
+                height={25}
+                className=" top-6 left-3"
+              />winners & Fulfillment</li>
+
+            <li className="mt-4 ml-2 w-full bg-white py-3 rounded-xl border-none 
+             font-normal text-black text-lg 
+             flex items-center  gap-2
+             hover:text-white hover:bg-[#F2482D]
+             hover:shadow-[3px_3px_0px_black]">
+              <Image
+                src="/gift.svg"
+                alt="Dashboard Icon"
+                width={25}
+                height={25}
+                className=" top-6 left-3"
+              />Weekly Giveaway</li>
+
+            <li className="mt-4 ml-2 w-full bg-white py-3 rounded-xl border-none 
+             font-normal text-black text-lg 
+             flex items-center  gap-2
+             hover:text-white hover:bg-[#F2482D]
+             hover:shadow-[3px_3px_0px_black]">
+              <Image
+                src="/disputes.svg"
+                alt="Dashboard Icon"
+                width={25}
+                height={25}
+                className=" top-6 left-3"
+              />Disputes</li>
+
+            <li className="mt-4 ml-2 w-full bg-white py-3 rounded-xl border-none 
+             font-normal text-black text-lg 
+             flex items-center  gap-2
+             hover:text-white hover:bg-[#F2482D]
+             hover:shadow-[3px_3px_0px_black]">
+              <Image
+                src="/revenue.svg"
+                alt="Dashboard Icon"
+                width={25}
+                height={25}
+                className=" top-6 left-3"
+              />Revenue Overview</li>
+
+            <li className="mt-4 ml-2 w-full bg-white py-3 rounded-xl border-none 
+             font-normal text-black text-lg
+             flex items-center  gap-2
+             hover:text-white hover:bg-[#F2482D]
+             hover:shadow-[3px_3px_0px_black]">
+              <Image
+                src="/banners.svg"
+                alt="Dashboard Icon"
+                width={25}
+                height={25}
+                className=" top-6 left-3"
+              />Manage Banners </li>
+
+          </ul>
+        </aside>
+      </div>
+
+
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="bg-white p-10 rounded-2xl shadow-lg w-full max-w-md text-center">
+
+          <h1 className="text-2xl font-bold mb-4 login-title">
+            Welcome to Dashboard 🎉
+          </h1>
+
+          <p className="text-gray-600 mb-6">You are successfully logged in.</p>
+
+          <button
+            onClick={handleLogout}
+            className="mt-4 w-full text-white bg-[#F2482D] hover:bg-[#d33c25] py-3 rounded-xl border border-black transition font-semibold flex items-center justify-center gap-2 shadow-[3px_3px_0px_black]"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
